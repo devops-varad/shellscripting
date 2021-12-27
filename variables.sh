36cf48450d6f6fcf792d16029b583a025a6bb698
devops-varad <devopsvarad@gmail.com>
2021-12-27 22:50:12 +0530

    declared variables syntax line added now

--- variables.sh
+++ variables.sh
@@ -5,4 +5,13 @@ b=welcome
 c=true
 #we can call the variable by $a or ${a}. Recommended way is using flower brackets
 echo value of a=$a
-echo value of a=USD$a
+echo value of a= ${a}USD
+#Variables will not be hard coded and we need a dynamic function
+DATE=27th-Dec-2021
+echo date is $(DATE)
+#Below value is an example of getting dynamic data
+DATE=$(date +%F)
+echo Now the date is ${DATE}
+##Arithmetic expression
+ADD=$((10+5))
+echo Addion of Arithmetic value is ${ADD}
