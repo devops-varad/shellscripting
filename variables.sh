34e65354ed196c8dfff9fb489e10eb67881d9eb6
devops-varad <devopsvarad@gmail.com>
2021-12-27 22:27:49 +0530

    declared variables syntax line added now

--- variables.sh
+++ variables.sh
@@ -3,4 +3,6 @@
 a=100
 b=welcome
 c=true
-#we can call the variable by $a or ${a}
+#we can call the variable by $a or ${a}. Recommended way is using flower brackets
+echo value of a=$a
+echo value of a=USD$a
