980587fa5718800d91b40db70b931e0c2ca9eeca
devops-varad <devopsvarad@gmail.com>
2021-12-28 20:14:55 +0530

    read syntax example script

---
+++ read.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#This script is to get input from user two ways, 1.During Execution 2.Before Execution
+read -p "enter your name:" name
+
+read -p "enter your age:" age
+
+echo "your name is $name and your age is ${age} "
