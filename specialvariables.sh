630843e88133dd9e94d00ab913c0d262bfb02aaa
devops-varad <devopsvarad@gmail.com>
2021-12-30 16:56:36 +0530

    Declared special variables with echo command

--- specialvariables.sh
+++ specialvariables.sh
@@ -4,16 +4,17 @@
 ## $1-$n=Arguments are parsed in the order
 ## $*,$@ - All arguments
 ## $# - Number of arguments
+#display the script name
 echo ${0}
-
+#display the 1st argument
 echo ${1}
-
+#display the 2nd argument
 echo ${2}
-
+#display the 3rd argument
 echo ${3}
-
+#diplsay all the arguments
 echo ${*}
 
 echo ${@}
-
+#display the total number of arguments
 echo ${#}
