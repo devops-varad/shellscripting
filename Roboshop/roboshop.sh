5cee6ca6085aa1f32bee9cb04d8c6540d4d46fb9
devops-varad <devopsvarad@gmail.com>
2021-12-31 18:32:13 +0530

    roboshop.sh script input variable error is fixed

--- Roboshop/roboshop.sh
+++ Roboshop/roboshop.sh
@@ -5,21 +5,21 @@
 # 3. Suppose user given wrong input, verify the available components list
 
 # To satisfy 1st condition:
-USER_ID=${id -u}
-if [ $(USER_ID) -ne 0 ]; then
+USER-ID=(id -u)
+if [ "${USER-ID}" -ne 0 ]; then
   echo -e "\e[1;31mExecute this script by root user \e[0m"
   exit
 fi
 
 #To satisfy 2nd condition:
 component=$1
-if [ -z ${component} ]; then
+if [ -z "$component" ]; then
   echo -e "\e[1;31mInput is Missing \e[0m"
   exit
 fi
 
 #To satisfy 3rd condition:
-if [ -e components/${component}.sh ]; then
+if [ -e components/"${component}".sh ]; then
   echo -e "\e[1;31mInvalid file name entered \e[0m"
   exit
 fi
